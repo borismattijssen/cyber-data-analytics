bb87ddb6edf302c83e09075ebda4e32ca8b0eccd
borismattijssen <borismattijssen@gmail.com>
2018-05-12 12:33:19 +0200

    Bug fixes

--- assignment1/smote.py
+++ assignment1/smote.py
@@ -10,15 +10,13 @@ from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.naive_bayes import GaussianNB as GNB
 from sklearn.tree import DecisionTreeClassifier as DTC
 
-def draw_roc(fig, X_train, X_test, Y_train, Y_test, title):
+def draw_roc(clf, X_train, X_test, Y_train, Y_test, title):
     # train classifier
     clf.fit(X_train,Y_train)
     Y_labels = clf.predict(X_test)
 
     # calculate roc curve
     fpr, tpr, thresholds = roc_curve(Y_test,Y_labels)
-
-
     plt.plot(fpr,tpr)
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
@@ -26,18 +24,13 @@ def draw_roc(fig, X_train, X_test, Y_train, Y_test, title):
     plt.ylabel('True Positive Rate')
     plt.title(title)
 
-def draw_rocs(clf, clf_name, X, Y):
-    # separate training and test set
-    X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2)
-
-    # perform SMOTE
-    sm = SMOTE()
-    X_res, Y_res = sm.fit_sample(X_train,Y_train)
-
+def draw_rocs(clf, clf_name, X_train, X_smote_train, X_test, Y_train, Y_smote_train, Y_test):
     fig = plt.figure()
     # create plot
-    draw_roc(fig, X_train, X_test, Y_train, Y_test, '%s UNSMOTEd' % clf_name)
-    draw_roc(fig, X_res, X_test, Y_res, Y_test, '%s SMOTEd' % clf_name)
+    plt.subplot(1,2,1)
+    draw_roc(clf, X_train, X_test, Y_train, Y_test, '%s UNSMOTEd' % clf_name)
+    plt.subplot(1,2,2)
+    draw_roc(clf, X_smote_train, X_test, Y_smote_train, Y_test, '%s SMOTEd' % clf_name)
 
     fig.savefig('ROC_%s.png' % clf_name)
 
@@ -55,100 +48,14 @@ y = data[:,data.shape[1]-1]
 X = np.asarray(x2,dtype=np.int32)
 Y = np.asarray(y,dtype=np.int32)
 
-draw_rocs(SGD(), 'SGD', X, Y)
+# separate training and test set
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,test_size=0.2)
 
+# perform SMOTE
+sm = SMOTE()
+X_smote_train, Y_smote_train = sm.fit_sample(X_train,Y_train)
 
-
-
-# use a random forest classifier with 10-fold cross-validation for both datasets
-# clf = RFC()
-# scores = cross_val_score(clf, X, Y, cv=10)
-# print(scores)
-
-# perform SMOTE and then repeat the above process
-# it's wrong, needs to separate training and test before using SMOTE
-
-#
-# clf = RFC()
-#
-# # train stochastic gradient descent classifier
-# clf = SGD()
-#
-# # same process for gaussian naive bayes classifier
-# clf = GNB()
-# clf.fit(X_train,Y_train)
-# Y_labels = clf.predict(X_test)
-#
-# fpr, tpr, thresholds = roc_curve(Y_test,Y_labels)
-#
-# plt.figure()
-# plt.plot(fpr,tpr,label='ROC curve')
-# plt.xlim([0.0, 1.0])
-# plt.ylim([0.0, 1.05])
-# plt.xlabel('False Positive Rate')
-# plt.ylabel('True Positive Rate')
-# plt.title('Receiver operating characteristic example')
-# plt.savefig('RocGNB.png')
-#
-# # same for decision tree classifier
-# clf = DTC()
-# clf.fit(X_train,Y_train)
-# Y_labels = clf.predict(X_test)
-#
-# fpr, tpr, thresholds = roc_curve(Y_test,Y_labels)
-#
-# plt.figure()
-# plt.plot(fpr,tpr,label='ROC curve')
-# plt.xlim([0.0, 1.0])
-# plt.ylim([0.0, 1.05])
-# plt.xlabel('False Positive Rate')
-# plt.ylabel('True Positive Rate')
-# plt.title('Receiver operating characteristic example')
-# plt.savefig('RocDTC.png')
-# # repeat for SMOTED data
-# X_train, X_test, Y_train, Y_test = train_test_split(X_res,Y_res,test_size=0.2)
-#
-# clf = SGD()
-# clf.fit(X_train,Y_train)
-# Y_labels = clf.predict(X_test)
-#
-# fpr, tpr, thresholds = roc_curve(Y_test,Y_labels)
-#
-# plt.figure()
-# plt.plot(fpr,tpr,label='ROC curve')
-# plt.xlim([0.0, 1.0])
-# plt.ylim([0.0, 1.05])
-# plt.xlabel('False Positive Rate')
-# plt.ylabel('True Positive Rate')
-# plt.title('Receiver operating characteristic example')
-# plt.savefig('RocSGDs.png')
-#
-# clf = GNB()
-# clf.fit(X_train,Y_train)
-# Y_labels = clf.predict(X_test)
-#
-# fpr, tpr, thresholds = roc_curve(Y_test,Y_labels)
-#
-# plt.figure()
-# plt.plot(fpr,tpr,label='ROC curve')
-# plt.xlim([0.0, 1.0])
-# plt.ylim([0.0, 1.05])
-# plt.xlabel('False Positive Rate')
-# plt.ylabel('True Positive Rate')
-# plt.title('Receiver operating characteristic example')
-# plt.savefig('RocGNBs.png')
-#
-# clf = DTC()
-# clf.fit(X_train,Y_train)
-# Y_labels = clf.predict(X_test)
-#
-# fpr, tpr, thresholds = roc_curve(Y_test,Y_labels)
-#
-# plt.figure()
-# plt.plot(fpr,tpr,label='ROC curve')
-# plt.xlim([0.0, 1.0])
-# plt.ylim([0.0, 1.05])
-# plt.xlabel('False Positive Rate')
-# plt.ylabel('True Positive Rate')
-# plt.title('Receiver operating characteristic example')
-# plt.savefig('RocDTCs.png')
+# draw_rocs(SGD(), 'SGD', X_train, X_smote_train, X_test, Y_train, Y_smote_train, Y_test)
+draw_rocs(RFC(), 'RFC', X_train, X_smote_train, X_test, Y_train, Y_smote_train, Y_test)
+draw_rocs(GNB(), 'GNB', X_train, X_smote_train, X_test, Y_train, Y_smote_train, Y_test)
+draw_rocs(DTC(), 'DTC', X_train, X_smote_train, X_test, Y_train, Y_smote_train, Y_test)
