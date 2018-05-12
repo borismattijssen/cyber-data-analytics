02b862165b065c106635f4a613725c1c8542623c
Michail Vrachasotakis <mikvrax@gmail.com>
2018-05-12 15:15:14 +0200

    Changed plots to include both curves in one figure

--- assignment1/smote.py
+++ assignment1/smote.py
@@ -10,28 +10,31 @@ from sklearn.model_selection import cross_val_score, train_test_split
 from sklearn.naive_bayes import GaussianNB as GNB
 from sklearn.tree import DecisionTreeClassifier as DTC
 
-def draw_roc(clf, X_train, X_test, Y_train, Y_test, title):
+def draw_roc(clf, X_train, X_test, Y_train, Y_test, color,label):
     # train classifier
     clf.fit(X_train,Y_train)
     Y_labels = clf.predict(X_test)
 
     # calculate roc curve
     fpr, tpr, thresholds = roc_curve(Y_test,Y_labels)
-    plt.plot(fpr,tpr)
+    # add roc curve in the figure with a different color and a legend label
+    plt.plot(fpr, tpr, color=color, label=label)
+
+def draw_rocs(clf, clf_name, X_train, X_smote_train, X_test, Y_train, Y_smote_train, Y_test):
+    fig = plt.figure()
+    # create plots
+    draw_roc(clf, X_train, X_test, Y_train, Y_test, 'darkorange','%s UNSMOTEd' % clf_name)
+    draw_roc(clf, X_smote_train, X_test, Y_smote_train, Y_test, 'navy','%s SMOTEd' % clf_name)
+
+    #set axis range, title and legend
     plt.xlim([0.0, 1.0])
     plt.ylim([0.0, 1.05])
     plt.xlabel('False Positive Rate')
     plt.ylabel('True Positive Rate')
-    plt.title(title)
+    plt.title('Comparison of SMOTE with ROC curves')
+    plt.legend(loc="lower right")
 
-def draw_rocs(clf, clf_name, X_train, X_smote_train, X_test, Y_train, Y_smote_train, Y_test):
-    fig = plt.figure()
-    # create plot
-    plt.subplot(1,2,1)
-    draw_roc(clf, X_train, X_test, Y_train, Y_test, '%s UNSMOTEd' % clf_name)
-    plt.subplot(1,2,2)
-    draw_roc(clf, X_smote_train, X_test, Y_smote_train, Y_test, '%s SMOTEd' % clf_name)
-
+    plt.show()
     fig.savefig('ROC_%s.png' % clf_name)
 
 
@@ -41,11 +44,11 @@ def draw_rocs(clf, clf_name, X_train, X_smote_train, X_test, Y_train, Y_smote_tr
 data = np.loadtxt('data/original_data.csv', delimiter=' ')
 
 # separate features from labels for the original and aggregated data sets
-x2 = data[:,:data.shape[1]-2]
+x = data[:,:data.shape[1]-2]
 y = data[:,data.shape[1]-1]
 
 # turn them into arrays
-X = np.asarray(x2,dtype=np.int32)
+X = np.asarray(x,dtype=np.int32)
 Y = np.asarray(y,dtype=np.int32)
 
 # separate training and test set
